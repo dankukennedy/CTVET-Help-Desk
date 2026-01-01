c9553401375c3ba7b0e4ee762c7bdc86a80546bb
dankukennedy <dankukennedy@users.noreply.github.com>
2026-01-01 16:06:40 +0000

    all configurations set

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+//import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // app.useGlobalPipes(new ValidationPipe({
+  //   whitelist:true,
+  //   forbidNonWhitelisted:true,
+  //   transform:true,
+  // }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Users example')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
