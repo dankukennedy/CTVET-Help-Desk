1504a7b7198c0220c8fdf1e79b88a1d42fca25d8
dankukennedy <dankukennedy@users.noreply.github.com>
2026-01-01 19:35:54 +0000

    git add

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { IsString, IsNotEmpty,IsEmail, IsNumber, IsEnum } from "class-validator"
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty({ message: 'Name should not be empty' })
+    name:string;
+
+    @IsEmail({},{ message: 'Please provide valid email' })
+    @IsNotEmpty()
+    email:string;
+
+    @IsNumber()
+    @IsNotEmpty( {message: 'Number cannot be left empty' })
+    contact:number;
+
+    @IsEnum(['ADMIN', 'USER', 'CUSTOMER', 'AGENT', 'INFOSEEK'],{ message: 'Role must be one of the following: ADMIN, USER, CUSTOMER, AGENT, or INFOSEEK'})
+    @IsNotEmpty()
+    role: 'ADMIN' | 'USER' | 'CUSTOMER' | 'AGENT' | 'INFOSEEK';
+}
